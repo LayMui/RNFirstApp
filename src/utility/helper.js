c3f7fa7516449f756a7dc4666bef1dd42a9c301a
LayMui <laymui.toh@taiger.com>
2021-03-15 13:45:26 +0800

    testProperties helper

--- src/utility/helper.js
+++ src/utility/helper.js
@@ -1,16 +1,15 @@
-/**
+**
  * Add a unique test id for iOS and Android
  *
  * @param {string} id
+ * @param {boolean} disableAccessible All touchable elements are accessible, meaning that it
+ * groups its children into a single selectable component, sometimes this is not needed for testing
  *
  * @return {object}
  */
 
-// import {Platform} from 'react-native';
+export function testProperties(id, disableAccessible = false) {
+  const disableAccessibility = disableAccessible ? { accessible: false } : {};
 
-// export const IS_IOS = Platform.OS === 'ios';
-
-export function testProperties(id) {
-    return {testID: id, accessibilityLabel: id};
+  return { ...disableAccessibility, accessibilityLabel: id };
 }
-  
